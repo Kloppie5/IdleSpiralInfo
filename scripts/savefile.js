6de38e8614add100a60bdeca3df870bfc49a3ae7
Gabriel Taets <gabrieltaets@gmail.com>
2022-10-28 02:48:39 +0300

    feat: exporting save files

--- scripts/savefile.js
+++ scripts/savefile.js
@@ -1,11 +1,13 @@
 
 var rawsavefile = ""
 var dto = {}
+var lastChar;
 
 async function loadSave ( event ) {
   const file = event.target.files.item(0)
   const contents = await file.text()
   rawsavefile = contents.slice(0, -1) // F*cking null character
+  lastChar = contents.charAt(-1);
   document.getElementById("savefile-input").value = rawsavefile
 
   var savefile = decryptSavefile(rawsavefile)
@@ -37,4 +39,39 @@ function decryptSavefile ( rawsavefile ) {
   }
 
   return savefile
+}
+function exportSavefile() {
+  const json = JSON.parse(document.getElementById("savefile-decrypted-full").value);
+
+  var key = CryptoJS.enc.Hex.parse("0901f87d5725efde4e4ae88cc197230b")
+  var iv = CryptoJS.enc.Hex.parse("14ce63b855acd907c451a1777e592457")
+
+  const data = json.map(part => {
+    part = JSON.stringify(part).replace(/e\+/g, "e");
+
+    part = CryptoJS.AES.encrypt(part, key, {
+      iv: iv,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7
+    });
+
+    return CryptoJS.enc.Base64.stringify(part.ciphertext);
+  }).join("#") + lastChar;
+
+  downloader(data, "text/plain", "Idle Spiral.txt");
+}
+
+function downloadURI(uri, name) {
+  let link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  link.click();
+  link.remove();
+}
+
+function downloader(data, type, name) {
+  let blob = new Blob([data], {type});
+  let url = window.URL.createObjectURL(blob);
+  downloadURI(url, name);
+  window.URL.revokeObjectURL(url);
 }
